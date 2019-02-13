12efd6069d4aa18460cc1b2894d5577de6de96ba
Marcus Gasberg <mrgasberg@hotmail.com>
2019-02-13 10:58:01 +0100

    Created first unit tests

--- ECS.Test.Unit/ECSTests.cs
+++ ECS.Test.Unit/ECSTests.cs
@@ -1,44 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ECS.Refactored;
 using NUnit.Framework;
-using ECS.Refactored;
 
 namespace ECS.Test.Unit
 {
-    public class HeaterFakeReturns : IHeater
-    {
-        public bool TurnOnCalled { get; private set; } = false;
-        public bool TurnOffCalled { get; private set; } = false;
-        public void TurnOn()
-        {
-            TurnOnCalled = true;
-        }
-
-        public void TurnOff()
-        {
-            TurnOffCalled = true;
-        }
-    }
-
-    public class TempSensorFake : ISensor
-    {
-        public int GetTemp()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     [TestFixture]
     public class ECSTests
     {
-        private Refactored.ECS ECSUnderTest;
+        private Refactored.ECS _ecsUUT;
+
         [SetUp]
         public void ECS_Setup()
         {
-            ECSUnderTest = new Refactored.ECS(new HeaterFakeReturns(), new TempSensorFake());
+            _ecsUUT = new Refactored.ECS(20, new TempSensorFake(), new HeaterFakeReturns());
+        }
+
+        [TestCase(20,20)]
+        [TestCase(0, 0)]
+        [TestCase(2, 2)]
+        [TestCase(-20,-20)]
+        public void ECS_SetThreshold_ThresholdCorrect(int treshold,int result)
+        {
+            _ecsUUT.SetThreshold(treshold);
+            Assert.That(_ecsUUT.GetThreshold(),Is.EqualTo(result));
         }
     }
 }
---
+++ ECS.Test.Unit/TestFakes.cs
@@ -0,0 +1,42 @@
+﻿using ECS.Refactored;
+
+namespace ECS.Test.Unit
+{
+    internal class HeaterFakeReturns : IHeater
+    {
+        public int TurnOnCalled { get; private set; } = 0;
+        public int TurnOffCalled { get; private set; } = 0;
+        public bool SelfTestResult { private get; set; } = false;
+
+        public void TurnOn()
+        {
+            //If turned off called?
+            TurnOnCalled++;
+        }
+
+        public void TurnOff()
+        {
+            //If turned on called?
+            TurnOffCalled++;
+        }
+
+        public bool RunSelfTest() => SelfTestResult;
+    }
+
+    internal class TempSensorFake : ISensor
+    {
+        public bool SelfTestResult { private get; set; } = false;
+        public int TemperatureResult { private get; set; } = 0;
+
+        public int GetTemp()
+        {
+            return TemperatureResult;
+        }
+
+        public bool RunSelfTest()
+        {
+            return SelfTestResult;
+        }
+    }
+
+}
